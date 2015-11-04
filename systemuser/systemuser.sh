30bab30dc8e66450ba0fd0dd98fb51d120d760f4
eschlon <eschlon@mac.com>
2015-11-04 13:43:50 -0800

    Sudo Access & Prep for --net=host

--- systemuser/systemuser.sh
+++ systemuser/systemuser.sh
@@ -2,9 +2,14 @@
 
 echo "Creating user $USER ($USER_ID)"
 useradd -u $USER_ID -s $SHELL $USER
+
+# Grant User Sudo Access in the Container
+usermod -aG sudo $USER
+echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/10-jupyter-user
+
 sudo -E -u $USER jupyterhub-singleuser \
-  --port=8888 \
-  --ip=0.0.0.0 \
+  --port=`expr $USER_ID + 30000` \
+  --ip=127.0.0.1 \
   --user=$JPY_USER \
   --cookie-name=$JPY_COOKIE_NAME \
   --base-url=$JPY_BASE_URL \
